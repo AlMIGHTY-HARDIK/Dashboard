68fe91db786afe4f8cf157b84813a1ad76c8de2c
HARDIK SOMKUWAR <120791564+AlMIGHTY-HARDIK@users.noreply.github.com>
2024-12-12 12:53:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,5 +192,7 @@ dashboard = ExplainerDashboard(
 server = FastAPI()
 server.mount("/", WSGIMiddleware(dashboard.flask_server()))
 
+
 if __name__ == '__main__':
-    uvicorn.run("app:server", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
+    uvicorn.run("app:server", host="0.0.0.0", port=port, reload=True)
